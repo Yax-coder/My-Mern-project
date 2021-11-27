2cfbd90a8f17c433abb42b9fc5ddedaf0a0e6164
yahaya <ahmadyahya962@gmail.com>
2021-11-27 23:35:19 +0100

    configured users endpoints

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,87 +1,84 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator');
 const gravatar = require('gravatar');
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const config = require('config');
+const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
 
-// @route   Post api/users
-// @desc    Register users route
-// @access  Public
-router.post('/', [
-     check('name', 'name is required').not().isEmpty(),
-     check('email', 'p;ease include a valid email').isEmail(),
-     check('password',
-          'p;ease enter a password with six or more characters')
-          .isLength({ min: 6 }),
+// @route    POST api/users
+// @desc     Register user
+// @access   Public
+router.post(
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-], async (req, res) => {
-     const errors = validationResult(req);
-     if (!errors.isEmpty()) {
-          res.status(400).json({ errors: errors.array() })
-     }
+    const { name, email, password } = req.body;
 
-     const { name, email, password } = req.body
+    try {
+      let user = await User.findOne({ email });
 
-     try {
-          // see if user exist
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
+      }
 
-          let user = await User.findOne({ email });
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+      });
 
-          if (user) {
-               return res.status(400)
-                    .json({ errors: [{ msg: 'User with this email already exist' }] })
-          }
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
 
-          // Get user gravatar
+      const salt = await bcrypt.genSalt(10);
 
-          const avatar = gravatar.url(email, {
-               s: "200",
-               r: "pg",
-               d: "mm"
-          })
+      user.password = await bcrypt.hash(password, salt);
 
-          user = new User({
-               name,
-               email,
-               avatar,
-               password
-          })
+      await user.save();
 
-          // Encrypt password
-          const salt = await bcrypt.genSalt(10);
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
 
-          user.password = await bcrypt.hash(password, salt);
-
-
-          await user.save();
-
-          // Return json-web-token
-          const payload = {
-               user: {
-                    id: user.id
-               }
-          }
-
-          jwt.sign(payload, config.get('jwtSecret'), {
-               expiresIn: 360000
-          },
-               (err, token) => {
-                    if (err) throw err;
-                    res.json({ token })
-               }
-          )
-
-     } catch (error) {
-          console.error(error.message);
-          res.status(500).send('server error')
-     }
-
-
-
-});
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
 
 module.exports = router;
