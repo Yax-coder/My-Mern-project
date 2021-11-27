07080bb4257920032a31361b4cf36bc1afeff247
yahaya <ahmadyahya962@gmail.com>
2021-11-27 23:32:42 +0100

    set up authentication with jwt

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,22 +1,27 @@
-const jwt = require('jsonwebtoken')
-const config = require('config')
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 module.exports = function (req, res, next) {
-     // get the token from header
-     const token = req.header('x-auth-token');
+  // Get token from header
+  const token = req.header('x-auth-token');
 
-     // check if there is no token
+  // Check if not token
+  if (!token) {
+    return res.status(401).json({ msg: 'No token, authorization denied' });
+  }
 
-     if (!token) {
-          return res.status(401).json({ msg: 'no token, access denied' });
-     }
-
-     try {
-          const decoded = jwt.verify(token, config.get('jwtSecret'));
-
-          req.user = decoded.user;
-          next();
-     } catch (error) {
-          res.status(401).json({ msg: 'token is not valid' })
-     }
-}
+  // Verify token
+  try {
+    jwt.verify(token, config.get('jwtSecret'), (error, decoded) => {
+      if (error) {
+        return res.status(401).json({ msg: 'Token is not valid' });
+      } else {
+        req.user = decoded.user;
+        next();
+      }
+    });
+  } catch (err) {
+    console.error('something wrong with auth middleware');
+    res.status(500).json({ msg: 'Server Error' });
+  }
+};
